e272c233465d39fb262ac88c2a54fe7d347e7421
benneifert <beifert@gmail.com>
2016-04-11 15:49:46 -0700

    test commit from pycharm

--- qfs_core.py
+++ qfs_core.py
@@ -2,6 +2,7 @@ import pandas as pd
 import datetime as dt
 import pandas_datareader.data as pdata
 import matplotlib.pyplot as plt
+import numpy as np
 
 data = pdata.get_data_yahoo(symbols="^GSPC",
                             start="01/01/2010",
@@ -11,4 +12,6 @@ plt.figure()
 data['Adj Close'].plot()
 plt.show()
 
-
+returns = data['Adj Close'] / data['Adj Close'].shift(1) - 1
+plt.figure()
+plt.hist(returns[np.isfinite(returns)])
