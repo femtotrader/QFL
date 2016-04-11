493d7ee00181cd14a3665194a3c7604733df488f
Benn Eifert <benneifert@Benns-MacBook-Pro.local>
2016-04-11 15:03:19 -0700

    made a change

--- qfs_core.py
+++ qfs_core.py
@@ -3,10 +3,12 @@ import datetime as dt
 import pandas_datareader.data as pdata
 import matplotlib.pyplot as plt
 
-data = pdata.get_data_yahoo(symbols="yhoo",
+data = pdata.get_data_yahoo(symbols="^GSPC",
                             start="01/01/2010",
                             end="01/01/2015")
 
 plt.figure()
 data['Adj Close'].plot()
 plt.show()
+
+
